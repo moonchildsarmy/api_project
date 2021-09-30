091957c8712ef3b44afa6b7ae35257b044f6c387
Jazgul <arzieva1313@gmail.com>
2021-09-30 10:23:55 +0600

    api work version with admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import News
+
 
 # Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    search_fields = ['title']
+
+
+admin.site.register(News, NewsAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=150, blank=True)
+
+
+    def __str__(self):
+        return self.title
 # Create your models here.
